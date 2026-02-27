55ad050dc14932179d4721c2572c18277073b1ce
Kartik-007 <masterkartik1@gmail.com>
2026-02-26 22:37:40 -0500

    Fix download URL to correct GitHub username

--- Casks/flowprompt.rb
+++ Casks/flowprompt.rb
@@ -2,10 +2,10 @@ cask "flowprompt" do
   version "1.0.0"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/kartikmehra/FlowPrompt/releases/download/v#{version}/FlowPrompt.zip"
+  url "https://github.com/Kartik-007/FlowPrompt/releases/download/v#{version}/FlowPrompt.zip"
   name "FlowPrompt"
   desc "Lightweight macOS menu bar app for storing and pasting prompts"
-  homepage "https://github.com/kartikmehra/FlowPrompt"
+  homepage "https://github.com/Kartik-007/FlowPrompt"
 
   depends_on macos: ">= :ventura"
 
