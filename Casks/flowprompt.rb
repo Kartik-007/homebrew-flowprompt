4148817d2393d1e18a291fe04d3266ce1a6e8bfe
Kartik-007 <masterkartik1@gmail.com>
2026-02-26 22:31:53 -0500

    Add FlowPrompt cask v1.0.0

---
+++ Casks/flowprompt.rb
@@ -0,0 +1,17 @@
+cask "flowprompt" do
+  version "1.0.0"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
+  url "https://github.com/kartikmehra/FlowPrompt/releases/download/v#{version}/FlowPrompt.zip"
+  name "FlowPrompt"
+  desc "Lightweight macOS menu bar app for storing and pasting prompts"
+  homepage "https://github.com/kartikmehra/FlowPrompt"
+
+  depends_on macos: ">= :ventura"
+
+  app "FlowPrompt.app"
+
+  zap trash: [
+    "~/.flowprompt",
+  ]
+end
